d1845bb640674e63cd53ac5a1a1945ae12868faf
Daniel Moore <dan@mooresolutions.io>
2024-06-24 08:49:00 -0500

    a few updates for prod: #1

--- ingest.sh
+++ ingest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
 # cd into the directory of the script
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit
+if [[ -z "${ACTIVATE_CONDA}" ]]; then
+  /home/ubuntu/miniconda3/bin/conda activate kapidev
+fi
+
 
 pip install -r requirements.txt -U
 pip install -r dev-requirements.txt -U
