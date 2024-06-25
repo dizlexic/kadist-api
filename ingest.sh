b3e57e0f32c0e809f268134d349031c51c0bd4da
Daniel Moore <dan@mooresolutions.io>
2024-06-25 04:45:14 -0500

    a few updates for prod: updated ingest and crons #9

--- ingest.sh
+++ ingest.sh
@@ -1,17 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash -l
 # cd into the directory of the script
 cd "$(dirname "$0")" || exit
-if [[ -z "${ACTIVATE_CONDA}" ]]; then
-  /home/ubuntu/miniconda3/bin/conda activate kapidev
-fi
 
+eval "$(conda shell.bash hook)"
 
 pip install -r requirements.txt -U
 pip install -r dev-requirements.txt -U
 
-#Videos from Program Pages (links)
 python kadist_videos.py
-#python external_videos.py
-#60 Second Interviews from People Pages (links)
+python external_videos.py
 python kview_videos.py
 python download_optimized.py
