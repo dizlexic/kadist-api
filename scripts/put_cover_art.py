bdef942a77123389bc7d1d2e3126ce66cbfde078
dizlexic <dan@mooresolutions.io>
2025-03-01 17:38:13 -0600

    Update source folder for cover image uploads to S3
    Changed the script to upload files from the "storage" folder instead of "kview_cover_images". This ensures file paths align with the updated directory structure.

--- scripts/put_cover_art.py
+++ scripts/put_cover_art.py
@@ -7,6 +7,6 @@ bucket_name = "arpedia-dev"
 
 s3helper = S3Helper(bucket_name)
 
-for file in glob.glob("kview_cover_images/*.png"):
+for file in glob.glob("storage/*.png"):
     fname = os.path.basename(file)
     s3helper.put_file(fname, file)
